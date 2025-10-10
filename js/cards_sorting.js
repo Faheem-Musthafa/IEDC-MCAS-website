be112e24ff0459736de7ddb9b9f90302a483fb85
Faheem-Musthafa <faheemcp241@gmail.com>
2025-10-10 22:43:37 +0530

    Update and Added Execom 24-25

--- js/cards_sorting.js
+++ js/cards_sorting.js
@@ -6,12 +6,15 @@
   
   function sortCards() {
     var cardsContainer = document.getElementById('point-content');
+    if (!cardsContainer) return; // nothing to sort on pages without the point table
     var cards = Array.from(cardsContainer.getElementsByClassName('point-card'));
   
     // Sort the cards based on the data-points attribute
     cards.sort(function(a, b) {
-      var pointsA = parseInt(a.querySelector('h2').getAttribute('data-points'));
-      var pointsB = parseInt(b.querySelector('h2').getAttribute('data-points'));
+      var aH2 = a.querySelector('h2');
+      var bH2 = b.querySelector('h2');
+      var pointsA = aH2 ? parseInt(aH2.getAttribute('data-points')) || 0 : 0;
+      var pointsB = bH2 ? parseInt(bH2.getAttribute('data-points')) || 0 : 0;
       return pointsB - pointsA;
     });
   
--- js/constants.js
+++ js/constants.js
@@ -1,68 +1,91 @@
-
-
 //constants
 
+var bca_point = 40094;
+var ba_english_point = 15750;
+var bba_point = 10200;
+var economics_point = 2630;
+var multimedia_point = 30930;
+var electronics_point = 21715;
+var psychology_point = 8665;
+var bcom_ca_point = 19125;
+var bcom_tt_point = 8175;
 
-var bca_point = 36994;
-var ba_english_point = 15500;
-var bba_point = 9270;
-var economics_point = 2130;
-var multimedia_point = 28530;
-var electronics_point = 18415;
-var psychology_point = 5715;
-var bcom_ca_point = 18725;
-var bcom_tt_point = 6825;
-
-const lastUpdatedDate = "22-04-2025";
-
+const lastUpdatedDate = "10-10-2025";
 
 var total_points = bca_point+electronics_point+multimedia_point+psychology_point+economics_point+bcom_ca_point+bcom_tt_point+ba_english_point+bba_point;
 
+// Assigning values (guarded: only update if elements exist on the page)
 
-
-// Assigning values
-
-document.getElementById('bca_point').innerText = bca_point;
-document.getElementById('electronics_point').innerText = electronics_point;
-document.getElementById('multimedia_point').innerText = multimedia_point;
-document.getElementById('psychology_point').innerText = psychology_point;
-document.getElementById('economics_point').innerText = economics_point;
-document.getElementById('bcom_ca_point').innerText = bcom_ca_point;
-document.getElementById('bcom_tt_point').innerText = bcom_tt_point;
-document.getElementById('ba_english_point').innerText = ba_english_point;
-document.getElementById('bba_point').innerText = bba_point;
-
-
-
-// main.js
-window.addEventListener('load', function () {
-    setCardData('bca_point', bca_point, (bca_point / total_points) * 100);
-
-    setCardData('electronics_point', electronics_point, (electronics_point / total_points) * 100);
-
-    setCardData('multimedia_point', multimedia_point, (multimedia_point / total_points) * 100);
-
-    setCardData('psychology_point', psychology_point, (psychology_point / total_points) * 100);
-
-    setCardData('economics_point', economics_point, (economics_point / total_points) * 100);
-
-    setCardData('bcom_ca_point', bcom_ca_point, (bcom_ca_point / total_points) * 100);
-
-    setCardData('bcom_tt_point', bcom_tt_point, (bcom_tt_point / total_points) * 100);
-
-    setCardData('ba_english_point', ba_english_point, (ba_english_point / total_points) * 100);
-
-    setCardData('bba_point', bba_point, (bba_point / total_points) * 100);
-});
-
-
-function setCardData(id, value, num) {
-    var element = document.getElementById(id);
-    if (element) {
-        element.setAttribute('data-points', value);
-        element.innerText = value;
-        var percentElement = element.parentNode.parentNode;
-        percentElement.style.setProperty('--num', num);
-    }
+function assignPointIfPresent(id, value) {
+  var el = document.getElementById(id);
+  if (el) el.innerText = value;
 }
 
+assignPointIfPresent("bca_point", bca_point);
+assignPointIfPresent("electronics_point", electronics_point);
+assignPointIfPresent("multimedia_point", multimedia_point);
+assignPointIfPresent("psychology_point", psychology_point);
+assignPointIfPresent("economics_point", economics_point);
+assignPointIfPresent("bcom_ca_point", bcom_ca_point);
+assignPointIfPresent("bcom_tt_point", bcom_tt_point);
+assignPointIfPresent("ba_english_point", ba_english_point);
+assignPointIfPresent("bba_point", bba_point);
+
+// main.js
+window.addEventListener("load", function () {
+  setCardData("bca_point", bca_point, (bca_point / total_points) * 100);
+
+  setCardData(
+    "electronics_point",
+    electronics_point,
+    (electronics_point / total_points) * 100
+  );
+
+  setCardData(
+    "multimedia_point",
+    multimedia_point,
+    (multimedia_point / total_points) * 100
+  );
+
+  setCardData(
+    "psychology_point",
+    psychology_point,
+    (psychology_point / total_points) * 100
+  );
+
+  setCardData(
+    "economics_point",
+    economics_point,
+    (economics_point / total_points) * 100
+  );
+
+  setCardData(
+    "bcom_ca_point",
+    bcom_ca_point,
+    (bcom_ca_point / total_points) * 100
+  );
+
+  setCardData(
+    "bcom_tt_point",
+    bcom_tt_point,
+    (bcom_tt_point / total_points) * 100
+  );
+
+  setCardData(
+    "ba_english_point",
+    ba_english_point,
+    (ba_english_point / total_points) * 100
+  );
+
+  setCardData("bba_point", bba_point, (bba_point / total_points) * 100);
+});
+
+function setCardData(id, value, num) {
+  var element = document.getElementById(id);
+  if (element) {
+    element.setAttribute("data-points", value);
+    element.innerText = value;
+    var percentElement = element.parentNode.parentNode;
+    percentElement.style.setProperty("--num", num);
+  }
+}
